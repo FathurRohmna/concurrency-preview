a6a1e93afaf918b33f86b7ae1ae4da6c4b5d42b5
Fathur Rohman <fr081938@gmail.com>
2024-11-25 20:35:57 +0700

    [feat] buffered channels

---
+++ channels/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func produce(result chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 1; i <= 10; i++ {
+		fmt.Println("Producing:", i)
+		result <- fmt.Sprintf("%d", i)
+	}
+	close(result)
+}
+
+func consume(result chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for num := range result {
+		fmt.Println(num)
+		fmt.Println("Consuming:", num)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	// Buffered: Transaction processes can be performed simultaneously according to the set capacity
+	// Unbuffered: Transaction processes will be done one by one, waiting for each process to finish before proceeding to the next
+	result := make(chan string, 5)
+
+	wg.Add(2)
+	go produce(result, &wg)
+	go consume(result, &wg)
+	
+
+	wg.Wait()
+}
